37c289b90630a70dd377d6b7816ae224495bde66
death667b <idaniel@spinnys.com>
2017-08-12 11:36:37 +1000

    Put the stub code in the wrong file

---
+++ components/ListPage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ScrollView, View, Text, Button } from 'react-native';
+import { withRouter } from 'react-router-native';
+
+class ListPage extends React.Component {
+
+    viewStyle = {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    };
+
+    render() {
+        return (
+            <View>
+                <ScrollView>
+                    <View style={ this.viewStyle } >
+                        // Content goes here !!
+                    </ View>
+                </ ScrollView>
+                <Button onPress={ this.createPost } title='Create Post' />
+            </View>
+        )
+    }
+
+    createPost = () => {
+        this.props.router.push('/create');
+    }
+}
