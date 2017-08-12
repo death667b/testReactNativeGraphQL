d223cbcbe4d5cbc1a0e7d701f24570fc861416cb
death667b <idaniel@spinnys.com>
2017-08-12 12:56:47 +1000

    Added apollo imports and css cleanup

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View} from 'react-native';
+import ApolloClient, {createNetworkInterface} from 'apollo-client';
+import {ApolloProvidor} from 'react-apollo';
+import {nativeHistory, Route, Router} from 'react-router-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+const networkInterface = createNetworkInterface({uri: 'https://api.graph.cool/simple/v1/cj68ly79e4bxr01084hnjvhtw'})
 
-const styles = StyleSheet.create({
-  container: {
+const client = new ApolloClient({networkInterface})
+
+const container = {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+}
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={container}>
+                <Text>Open up App.js to start working on your app!</Text>
+                <Text>Changes you make will automatically reload.</Text>
+                <Text>Shake your phone to open the developer menu.</Text>
+            </View>
+        );
+    }
+}
