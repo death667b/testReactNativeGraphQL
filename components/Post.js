ecaf5a7d62f29efc51a57e87a0a6e5346e908de8
death667b <idaniel@spinnys.com>
2017-08-12 12:21:15 +1000

    Post stub code entered

---
+++ components/Post.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+export default class Post extends React.Component {
+    
+    static propTypes = {
+        post: React.PropTypes.object,
+    }
+
+    styleComponent = {
+        flex: 1,
+        width: 150,
+        height: 150,
+    }
+    
+    render() {
+        return (
+            <View style={ this.styleComponent }>
+                <Image 
+                    style={ this.styleComponent }
+                    source= {{ uri: this.props.post.imageUrl }}
+                /> 
+                <Text>{this.props.post.description}</Text>
+            </ View>
+        )
+    }
+}
